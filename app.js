94711b5b10147de61470269a72a8863bfc39d6e5
Ankur Patel <ankurpatel544@gmail.com>
2025-03-19 13:40:35 -0700

    Update calling out winner

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-'use strict';
+"use strict";
 
 // Selecting Elements - Allows us to declare once and reuse throughout the code
-const player0El = document.querySelector('.player--0');
-const player1El = document.querySelector('.player--1');
-const score0El = document.getElementById('score--0');
-const score1El = document.getElementById('score--1');
-const current0El = document.getElementById('current--0');
-const current1El = document.getElementById('current--1');
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+const score0El = document.getElementById("score--0");
+const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
 
-const diceEl = document.querySelector('.dice');
-const btnNew = document.querySelector('.btn--new');
-const btnRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn--hold');
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 
 let scores, currentScore, activePlayer, playing;
 
@@ -27,11 +27,16 @@ const init = function () {
   score0El.textContent = 0;
   score1El.textContent = 0;
 
-  diceEl.classList.add('hidden');
-  player0El.classList.remove('player--winner');
-  player1El.classList.remove('player--winner');
-  player0El.classList.add('player--active');
-  player1El.classList.remove('player--active');
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+  btnRoll.classList.remove("hidden");
+  btnHold.classList.remove("hidden");
+
+  document.getElementById("name--0").textContent = "Player 1";
+  document.getElementById("name--1").textContent = "Player 2";
 };
 
 // initialize the game parameters
@@ -42,18 +47,18 @@ const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   currentScore = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
-  player0El.classList.toggle('player--active');
-  player1El.classList.toggle('player--active');
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
 };
 
 // Rolling Dice Functionality
-btnRoll.addEventListener('click', function () {
+btnRoll.addEventListener("click", function () {
   if (playing) {
     // 1. generate random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
     console.log(dice); // Delete Later
     // 2. display roll
-    diceEl.classList.remove('hidden');
+    diceEl.classList.remove("hidden");
     diceEl.src = `dice-${dice}.png`;
 
     // 3. check if a 1 is rolled, if true switch to next player
@@ -70,7 +75,7 @@ btnRoll.addEventListener('click', function () {
 });
 
 // Hold Current Score Functionality
-btnHold.addEventListener('click', function () {
+btnHold.addEventListener("click", function () {
   if (playing) {
     //1. Add Current score to the active players score
     scores[activePlayer] += currentScore;
@@ -78,18 +83,23 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     //2. check if the players score is >= 100
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= 25) {
       playing = false;
       current0El;
-      diceEl.classList.add('hidden');
+      document.getElementById(`name--${activePlayer}`).textContent = `Player ${
+        activePlayer + 1
+      } Wins!`;
+      diceEl.classList.add("hidden");
+      btnHold.classList.add("hidden");
+      btnRoll.classList.add("hidden");
       // Assign player--winner styling
       document
         .querySelector(`.player--${activePlayer}`)
-        .classList.add('player--winner');
+        .classList.add("player--winner");
       // removes active--player class to apply winner css
       document
         .querySelector(`.player--${activePlayer}`)
-        .classList.remove('player--active');
+        .classList.remove("player--active");
     } else {
       // Switch to next player
       switchPlayer();
@@ -98,4 +108,4 @@ btnHold.addEventListener('click', function () {
 });
 
 // DRY implementation - New Game Button Functionality
-btnNew.addEventListener('click', init);
+btnNew.addEventListener("click", init);
